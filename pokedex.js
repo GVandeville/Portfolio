fa46eb20d6532ceabcc1c636d5a89362b2a221d2
Gvandeville <velmi.velmidos@hotmail.com>
2022-07-26 00:20:58 +0200

    Major javascript fix | Two clicks issue solved | Transformed div to buttons

--- pokedex.js
+++ pokedex.js
@@ -3,42 +3,49 @@ function playA() {
     var audio = document.getElementById("audioA");
     audio.play();
 }
+
 function playB() {
     var audio = document.getElementById("audioB");
     audio.play();
 }
 
-//Recognize the ID of each blue button and show the corresponding audio/image.
-function showImage() {
-    millisecondsToWait = 250;
-
-    document.querySelectorAll('#blueButtons1').forEach(blueButton => {
-        document.getElementById('1').onclick = () => {
-            var audio = document.getElementById("audioA");
-            audio.play();
-            setTimeout(function () { document.querySelector('.popup-image img').src = "images/philippines-monument.png" }, millisecondsToWait);
-            setTimeout(function () { document.querySelector('.popup-image').style.display = 'block' }, millisecondsToWait);
-        }
-
-        document.getElementById('2').onclick = () => {
-            var audio = document.getElementById("audioA");
-            audio.play();
-            setTimeout(function () { document.querySelector('.popup-image img').src = "images/indonesia-monument.jpg" }, millisecondsToWait);
-            setTimeout(function () { document.querySelector('.popup-image').style.display = 'block' }, millisecondsToWait);
-        }
-
-        document.getElementById('3').onclick = () => {
-            var audio = document.getElementById("audioA");
-            audio.play();
-            setTimeout(function () { document.querySelector('.popup-image img').src = "images/malaysia-monument.jpg" }, millisecondsToWait);
-            setTimeout(function () { document.querySelector('.popup-image').style.display = 'block' }, millisecondsToWait);
-        }
-    });
-
-    //Possibility to quit the image
-    document.querySelector('.popup-image span').onclick = () => {
-        document.querySelector('.popup-image').style.display = 'none';
-    }
+function showPicture1() {
+    playA();
+    millisecondsToWait = 150;
+    setTimeout(function () { document.querySelector('.popup-image img').src = "images/philippines-monument.png" }, millisecondsToWait);
+    setTimeout(function () { document.querySelector('.popup-image').style.display = 'block' }, millisecondsToWait);
 }
 
+function showPicture2() {
+    playA();
+    millisecondsToWait = 150;
+    setTimeout(function () { document.querySelector('.popup-image img').src = "images/indonesia-monument.jpg" }, millisecondsToWait);
+    setTimeout(function () { document.querySelector('.popup-image').style.display = 'block' }, millisecondsToWait);
+}
 
+function showPicture3() {
+    playA();
+    millisecondsToWait = 150;
+    setTimeout(function () { document.querySelector('.popup-image img').src = "images/malaysia-monument.jpg" }, millisecondsToWait);
+    setTimeout(function () { document.querySelector('.popup-image').style.display = 'block' }, millisecondsToWait);
+}
+
+let btn = document.getElementById("1");
+btn.addEventListener('click', event => {
+    showPicture1();
+});
+
+let btn2 = document.getElementById("2");
+btn2.addEventListener('click', event => {
+    showPicture2();
+});
+
+let btn3 = document.getElementById("3");
+btn3.addEventListener('click', event => {
+    showPicture3();
+});
+
+//Possibility to quit the image
+document.querySelector('.popup-image span').onclick = () => {
+    document.querySelector('.popup-image').style.display = 'none';
+}
